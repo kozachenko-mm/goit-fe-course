479dbceb891ee596a58f73f6d935b1668807b3f5
Michael Kozachenko <Kozachenko.Michael@gmail.com>
2019-02-12 22:39:13 +0200

    jshw-01

--- javascript/module-01/app.js
+++ javascript/module-01/app.js
@@ -1,1 +1,23 @@
-console.log("2 + 2");
+"use strict";
+
+const userLogin = prompt("Enter Login");
+
+if (userLogin === null) {
+  alert("Отменено пользователем!");
+}
+if (userLogin !== "admin") {
+  alert("Доступ запрещен, неверный логин!");
+}
+if (userLogin === "admin") {
+  const userPassword = prompt("Enter Password");
+
+  if (userPassword === null) {
+    alert("Отменено пользователем!");
+  }
+  if (userPassword !== "m4ngo1zh4ackz0r") {
+    alert("Доступ запрещен, неверный пароль!");
+  }
+  if (userPassword === "m4ngo1zh4ackz0r") {
+     alert("Добро пожаловать!");
+  }
+}
